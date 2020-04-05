c77e653db2a8f27c46aab8d6c24e9caae73be373
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-04-05 12:21:31 -0400

    added photon transform view to player prefab

---
+++ Assets/_FPSProject/Scripts/Weapons/Projectile Collision Scripts/SpawnObjectAtCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectAtCollision : MonoBehaviour, IProjectile_Collision
+{
+
+    private ObjectPooler m_pooler;
+    public GameObject m_spawnedObject;
+    private void Start()
+    {
+        m_pooler = ObjectPooler.instance;
+    }
+    public void ActivateCollision(GameObject p_collidedObject, Vector3 p_hitPosition)
+    {
+        m_pooler.NewObject(m_spawnedObject, p_hitPosition, Quaternion.identity);
+    }
+
+}
